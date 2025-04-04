f524e30f6edde5b24861819ea86b88f7904e8507
srivatt18 <srivattikuti90@gmail.com>
2025-04-04 18:19:57 -0500

    fixed gradle for the hashing algo

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,12 @@ repositories {
 }
 
 dependencies {
+    // JUnit for testing
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Spring Security Crypto for Argon2PasswordEncoder
+    implementation("org.springframework.security:spring-security-crypto:6.2.2")
 }
 
 tasks.test {
