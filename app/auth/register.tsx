0f069928157e5a8ada397bee8dead9f7e77723fd
sofiathomas715 <sofiathomas2901@gmail.com>
2025-04-12 14:59:42 -0500

    homepage frontend with basic redirection

--- app/auth/register.tsx
+++ app/auth/register.tsx
@@ -1,12 +1,12 @@
 // MyComponent.js
 import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet, Text } from 'react-native';
 
 export default function Register() {
   return (
-    <View style={styles.container}>
-      <TextInput style={styles.text}/>
-    </View>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Text>Sign-up Screen</Text>
+        </View>
   );
 };
 
