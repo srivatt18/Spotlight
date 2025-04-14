fefc77b3636cb62cb6d76dd1e3df06e0db56b66b
Siddharth <siddharth.060605@gmail.com>
2025-04-14 01:35:29 -0500

    Finally fix CORS error, start styling

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,43 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors"
 import { serve } from '@hono/node-server'
-import { auth } from "../lib/auth"; // path to your auth file
-import { web_serve } from "./web_serve";
+import { auth } from "lib/auth"; // path to your auth file
+// import { web_serve } from "./web_serve.ts";
 import { add_media, get_media, del_media } from "./api";
- 
+
 const app = new Hono();
- 
+
 console.log(auth.options)
 
-app.all("/api/auth/**", (c) => {console.log("got a request for auth"); let resp = auth.handler(c.req.raw); resp.then((a)=> console.log(a)); return resp});
+
+app.use(
+	"/api/auth/**", // or replace with "*" to enable cors for all routes
+	cors({
+		origin: (origin, _) => { // TODO: DO NOT LEAVE IN FINAL APP THIS IS VERY BAD
+            return origin;
+        },
+
+		allowHeaders: ["Content-Type", "Authorization"],
+		allowMethods: ["POST", "GET", "OPTIONS"],
+		exposeHeaders: ["Content-Length"],
+		maxAge: 600,
+		credentials: true,
+	}),
+);
+
+app.on(["POST", "GET"], "/api/auth/**", (c) => {
+    console.log("got a request for auth:");
+    console.log(c.req.raw.url)
+
+    let resp = auth.handler(c.req.raw);
+
+    resp.then((a) => {
+        console.log("The response from authhandler is:");
+        console.log(a);
+    });
+
+    return resp
+});
 
 app.get("/api/media/:uuid", (c) => c.json(get_media(c.req.param('uuid'))));
 app.delete("/api/media/:uuid", (c) => c.json(del_media(c.req.param('uuid'))))
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/react"
 export const { signIn, signUp, useSession } = createAuthClient(
     {
         /** the base url of the server (optional if you're using the same domain) */
-        baseURL: "http://127.0.0.1:3000"
+        baseURL: "http://localhost:3000"
     }
 )
